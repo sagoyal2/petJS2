942623062f59d709196c5671f5b30dbefc353da9
Samaksh Goyal <mailsamakshgoyal@gmail.com>
2019-07-23 16:24:55 -0700

    add new project

---
+++ src/MyFirst.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function MyFirst() {
+  return (<div><h1>Hello World</h1></div>)
+}
+
+export default MyFirst
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+import MyFirst from './MyFirst'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function MySecond() {
+  return (
+      <ul><li>1 < /li>
+      <li>2</li > </ul>)
+}
+
+ReactDOM.render(<MyFirst />,
+      document.getElementById('root'))
+  ReactDOM.render(<MySecond />, document.getElementById('not_root'))
+
+
+  console.dir(document);
+  console.log(document.domain)
