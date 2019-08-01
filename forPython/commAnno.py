dc75458473917fe46174ee0d00ee586fab277648
Samaksh Goyal <mailsamakshgoyal@gmail.com>
2019-08-01 00:46:33 -0700

    see classes and tags

--- forPython/commAnno.py
+++ forPython/commAnno.py
@@ -1,6 +1,31 @@
 import re
+import json
+import pprint
 import requests
 from bs4 import BeautifulSoup
+
+
+def createStructure(soup):
+    # mylist = []
+    # count = 0
+
+    # for tag in soup.find_all():
+
+    #     mylist.append(
+    #         (tag.name, tag.attrs, [parent.name for parent in tag.parents if parent.name != '[document]']))
+
+    #     if count > 10:
+    #         break
+    #     count += 1
+
+    mylist = [({'tag_name': tag.name}, {"tag_attrs": tag.attrs}, {"tag_parents": [parent.name for parent in tag.parents if parent.name !=
+                                                                                  '[document]']}) for tag in soup.find_all()]
+
+    open_file = open("savedlist.json", 'w')
+    open_file.write(json.dumps(mylist))
+    open_file.close()
+
+    pprint.pprint(mylist)
 
 
 def main():
@@ -10,15 +35,18 @@ def main():
     page_response = requests.get(page_link, timeout=5)
     soup = BeautifulSoup(page_response.content, "html.parser")
 
-    # file1 = open("allwiki.html", 'w')
-    # file1.write(str(soup.prettify))
-    # file1.close()
+    # createStructure(soup)
 
-    # result = soup.find_all(
-    #     'div', {"class": re.compile(r'nav\w+')})
+    json_file = open("savedlist.json", "r")
+    data = json.load(json_file)
+    json_file.close()
 
-    mylist = [(tag.name, tag.attrs) for tag in soup.find_all()]
-    print(mylist)
+    # print(data[27])
+    # print('class' in data[27][1].get("tag_attrs").keys())
+
+    for i in range(len(data)):
+        if('class' in data[i][1].get("tag_attrs").keys()):
+            print(data[i])
 
 
 if __name__ == "__main__":
